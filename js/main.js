41137da123456c6bdbc246aa59c3e678c02e39bf
Dan Kaplun <dan@beardtree.com>
2014-07-25 17:34:59 -0500

    Updates Google Analytics

--- js/main.js
+++ js/main.js
@@ -22,11 +22,11 @@ require(['jquery', 'bootstrap/tooltip'], function ($) {
   });
 
   // Google Analytics
-  var _gaq = _gaq || [['_setAccount', 'UA-32733609-1'], ['_trackPageview']];
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', 'UA-53185327-1', 'auto');
+  ga('send', 'pageview');
 });
