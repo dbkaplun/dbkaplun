b28751063022c3be96c75aed8e76dbb07deb6e7a
Dan Kaplun <dbkaplun@gmail.com>
2015-05-31 12:48:15 -0400

    feat(general): Adds Google Analytics

--- build.js
+++ build.js
@@ -4,20 +4,23 @@ var resume = require('./resume');
 var theme = require('jsonresume-theme-briefstrap');
 var pdf = require('html-pdf');
 var Promise = require('bluebird');
+var isogram = require('isogram');
 var fs = Promise.promisifyAll(require('fs'));
 
 build.opts = {
+  js: {append: isogram({id: 'UA-63592021-1'})},
   pdf: {
     filename: './resume.pdf',
     border: {top: '.75in', right: '.5in', bottom: '.75in', left: '.5in'}
   }
 
 };
-function build () {
-  return theme.render(resume, {less: {rootpath: 'node_modules/jsonresume-theme-briefstrap'}}).then(function (html) {
+function build (opts) {
+  opts = opts || build.opts;
+  return theme.render(resume, opts).then(function (html) {
     return Promise.all([
       fs.writeFileAsync('index.html', html),
-      Promise.promisifyAll(pdf.create(html, build.opts.pdf)).toFileAsync()
+      Promise.promisifyAll(pdf.create(html, opts.pdf)).toFileAsync()
     ]);
   });
 };
