96d7780cd82e1befa0f4e348ec96ae9c22a53088
Dan Kaplun <dbkaplun@gmail.com>
2015-05-30 23:07:09 -0400

    Adds border to PDF export

--- build.js
+++ build.js
@@ -7,15 +7,17 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 
 build.opts = {
-  pdf: {filename: './resume.pdf'}
+  pdf: {
+    filename: './resume.pdf',
+    border: {top: '.75in', right: '.5in', bottom: '.75in', left: '.5in'}
+  }
+
 };
 function build () {
   return theme.render(resume, {less: {rootpath: 'node_modules/jsonresume-theme-briefstrap'}}).then(function (html) {
     return Promise.all([
       fs.writeFileAsync('index.html', html),
-      Promise.promisifyAll(pdf.create(html, build.opts))
-        .toBufferAsync()
-        .then(fs.writeFileAsync.bind(null, 'resume.pdf'))
+      Promise.promisifyAll(pdf.create(html, build.opts.pdf)).toFileAsync()
     ]);
   });
 };
