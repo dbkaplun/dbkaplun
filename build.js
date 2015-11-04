0a35fbdde7eab01cfe87d3acd92c9d9c67951111
Dan Kaplun <dbkaplun@gmail.com>
2015-11-04 02:09:06 -0500

    fix(misc): stuff
    jsonresume-theme-briefstrap@0.2.4
    merge opts properly
    return obj instead of array from build

--- build.js
+++ build.js
@@ -6,22 +6,26 @@ var pdf = require('html-pdf');
 var Promise = require('bluebird');
 var isogram = require('isogram');
 var fs = Promise.promisifyAll(require('fs'));
+var _ = require('lodash');
 
-build.opts = {
-  js: {append: isogram({id: 'UA-63592021-1'})},
+build.DEFAULT_OPTS = {
   pdf: {
     filename: './resume.pdf',
     border: {top: '.75in', right: '.5in', bottom: '.75in', left: '.5in'}
+  },
+  prerender: function (result) {
+    result.js += isogram({id: 'UA-63592021-1'});
+    return result;
   }
-
 };
+
 function build (opts) {
-  opts = opts || build.opts;
+  opts = _.merge({}, build.DEFAULT_OPTS, opts);
   return theme.render(resume, opts).then(function (html) {
-    return Promise.all([
-      fs.writeFileAsync('index.html', html),
-      Promise.promisifyAll(pdf.create(html, opts.pdf)).toFileAsync()
-    ]);
+    return Promise.props({
+      html: fs.writeFileAsync('index.html', html),
+      pdf: Promise.promisifyAll(pdf.create(html, opts.pdf)).toFileAsync()
+    });
   });
 };
 
